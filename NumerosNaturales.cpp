9a9e92a470fc805aee9f084e28dafec860582998
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-04-18 11:18:08 -0600

    Create NumerosNaturales.cpp

---
+++ NumerosNaturales.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int start, end;
+	cin >> start >> end;
+
+	for(int i = start; i <= end; i++){
+		cout << i << endl;
+	}
+
+ 	return 0;
+}
