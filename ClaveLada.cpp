601b757257716de48b4492dfe8b0cdaf0509c428
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-04-18 11:19:27 -0600

    Create ClaveLada.cpp

---
+++ ClaveLada.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int lada;
+  cin >> lada;
+
+  if(lada == 473){
+    cout << "GUANAJUATO" << endl;
+  } else if(lada == 33){
+    cout << "GUADALAJARA" << endl;
+  } else if(lada == 445){
+    cout << "YURIRIA" << endl;
+  } else if(lada == 222){
+    cout << "PUEBLA" << endl;
+  } else if(lada == 477){
+    cout << "LEON" << endl;
+  } else if(lada == 442){
+    cout << "QUERETARO" << endl;
+  } else if(lada == 664){
+    cout << "TIJUANA" << endl;
+  } else if(lada == 722){
+    cout << "TOLUCA" << endl;
+  } else {
+    cout << "NO ENCONTRADO" << endl;
+  }
+  
+  return 0;
+}
