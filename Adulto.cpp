115321f9fe23334c3ee530379b0a2a0fe58fb96e
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-04-18 11:22:15 -0600

    Create Adulto.cpp

---
+++ Adulto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int age;
+  cin >> age;
+
+  if(age >= 18){
+    cout << "adulto" << endl;
+  } else {
+    cout << "joven" << endl;
+  }
+  return 0;
+}
