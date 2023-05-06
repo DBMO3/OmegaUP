fb3e384c18dc28958b49474a22ddeba75f2059e9
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-05-05 19:29:41 -0600

    Create DFD-Aceptado Rechazado.cpp

---
+++ DFD-Aceptado Rechazado.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  double a;
+  cin >> a;
+
+  if(a > 1.70){
+    cout << "Aceptado" << endl;
+  } else {
+    cout << "Rechazado" << endl;
+  }
+
+  return 0;
+}
