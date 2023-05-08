0ab8108de2c9a748f43e7881f67b1a4736206c6a
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-05-07 19:13:15 -0600

    Create Asegurando el perimetro.java

---
+++ Asegurando el perimetro.java
@@ -0,0 +1,80 @@
+class program {
+    program () {
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        move();
+        iterate(3) putbeeper();
+        
+        iterate(3) turnleft();
+        
+       	move();
+        iterate(2) putbeeper();
+        move();
+        iterate(2) putbeeper();
+        move();
+        iterate(2) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        iterate(2) putbeeper();
+        move();
+        iterate(2) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        iterate(2) putbeeper();
+        move();
+        iterate(2) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        iterate(2) putbeeper();
+        
+        iterate(3) turnleft();
+        
+        move();
+        putbeeper();
+        
+        turnoff();
+    }
+}
