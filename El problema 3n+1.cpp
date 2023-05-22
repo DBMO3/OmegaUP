c1186afbd39d37fafec0752e7bec5df89ddfcd64
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-05-21 21:37:04 -0600

    Update El problema 3n+1.cpp

--- El problema 3n+1.cpp
+++ El problema 3n+1.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     int n;
-    cin >> n;
+    cin >> n; // El usuario ingresa el valor N
+
     int steps = 0;
     int max_n = n;
     while (n != 1) {
         if (n % 2 == 0) {
-            // Si N es par, divídelo entre 2.
+            // Si N es par, se divide entre 2.
             n /= 2;
         } else {
-            // Si N es impar, multiplícalo por 3 y súmale 1.
+            // Si N es impar, se multiplica por 3 y se suma 1.
             n = n * 3 + 1;
         }
-        steps++;
+        steps++; // Cuenta los pasos
         if (n > max_n) {
             // Actualiza el valor más grande que toma N durante el cálculo
             max_n = n;
         }
     }
-    cout << steps << " " << max_n << endl;
+    cout << steps << " " << max_n << endl; // Muestra los pasos y el valor maximo
     return 0;
 }
