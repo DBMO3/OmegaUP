8d2cca81e51992121e60edd36a1af8a96040264f
Dylan Bernal Mora <121000809+DBMO3@users.noreply.github.com>
2023-05-21 21:59:46 -0600

    Create Secuencia creciente.cpp

---
+++ Secuencia creciente.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+	int count = 0;
+	int prevN = INT_MIN;
+	int currentN;
+	
+	while(true){
+		cin >> currentN;
+		
+		if(currentN < prevN){
+			break;
+		}
+		
+		count++;
+		prevN = currentN;
+	}
+	
+	cout << count << endl;
+}
